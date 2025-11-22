b04c46d9446ae9d5aa0202f12a5ac050fe5bdbd0
Ritik2004 <hritikgaur44@gmail.com>
2025-11-22 14:06:05 +0530

    complete code for quiz

---
+++ Frontend/src/Components/Right.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ChatArea from './ChatArea.jsx'
+const Right = () => {
+  return (
+    <div style={{width:"80%"}}>
+      <div style={{display:"flex", justifyContent:"center", alignItems:"center", height:"10%", borderBottom:"1px solid white",padding:"10px"}}>
+        <h1 style={{color: "white"}}>Welcome to Brain Battle</h1>
+        </div>
+      <div style={{height: "90%", overflow: "hidden"}}>
+  <ChatArea/>
+</div>
+    </div>
+  )
+}
+
+export default Right
