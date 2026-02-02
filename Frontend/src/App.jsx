06995df084efbb8f9d08759e1dd3870977a1056c
Ritik2004 <hritikgaur44@gmail.com>
2026-02-02 22:46:12 +0530

    added routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,11 @@
 import Left from './Components/Left.jsx'
 import Right from './Components/Right.jsx'
 import React from 'react'
+import {Routes,Route,Navigate} from 'react-router-dom'
+import Loginpage from './pages/Loginpage.jsx'
+import Homepage from './pages/Homepage.jsx'
+import Profilepage from './pages/Profilepage.jsx'
+import Signuppage from './pages/Signuppage.jsx'
 import './App.css'
 function App() {
  
@@ -8,7 +13,14 @@ function App() {
   return (
     <div div style={{display:"flex", height:"100vh", width:"100vw",overflow:"hidden"}}>
     {/* <Left/> */}
-    <Right/>
+    {/* <Right/> */}
+    <Routes>
+      <Route path='/' element={<Navigate to="/login"/>}/>
+      <Route path='/login' element={<Loginpage/>}/>
+      <Route path='/home' element={<Right/>}/>
+      <Route path='/profile' element={<Profilepage/>}/>
+      <Route path='/signup' element={<Signuppage/>}/>
+    </Routes>
     </div>
   )
 }
